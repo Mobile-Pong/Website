caf9dae2300944a345aff2a513f4aadecd1bbdcb
varung97 <varung97@yahoo.co.in>
2017-01-22 00:44:27 +0800

    Basic server API work

--- app.py
+++ app.py
@@ -18,9 +18,27 @@ def pushData():
     motion = request.args.get('motion', '')
     orientation = request.args.get('orientation', '')
 
+    cur.execute('UPDATE player_info \
+            SET mot_x = %s, mot_y = %s, mot_z = %s, ori_x = %s, ori_y = %s, ori_z = %s \
+            WHERE player_id = %s \
+        ',
+        (motion[0], motion[1], motion[2], orientation[0], orientation[1], orientation[2], playerID)
+    )
+    conn.commit()
+    conn.close()
+
 @app.route('/getData', methods=['POST'])
 def getData():
-    return json.dumps({'Status': 'Success'})
+    data = json.loads(request.data.decode())
+
+    conn = db_connection()
+    cur = db_cursor(conn)
+
+    cur.execute('SELECT * FROM player_info WHERE player_id = %s;', (data['player_id'], ))
+    return_val = dict(cur.fetchone())
+    conn.close()
+
+    return json.dumps(return_val)
 
 
 if __name__ == '__main__':
