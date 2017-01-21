5719f589593fdb455514bd05669c4308b1fc3eb2
varung97 <varung97@yahoo.co.in>
2017-01-22 04:15:12 +0800

    Change get to post

--- app.py
+++ app.py
@@ -9,18 +9,20 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/pushData', methods=['GET'])
+@app.route('/pushData', methods=['POST'])
 def pushData():
     conn = db_connection()
     cur = db_cursor(conn)
 
-    playerID = request.args.get('player_id', '')
-    mot_x = request.args.get('mot_x', '')
-    mot_y = request.args.get('mot_y', '')
-    mot_z = request.args.get('mot_z', '')
-    ori_x = request.args.get('ori_x', '')
-    ori_y = request.args.get('ori_y', '')
-    ori_z = request.args.get('ori_z', '')
+    data = json.loads(request.data.decode())
+
+    playerID = data['player_id']
+    mot_x = data['mot_x']
+    mot_y = data['mot_y']
+    mot_z = data['mot_z']
+    ori_x = data['ori_x']
+    ori_y = data['ori_y']
+    ori_z = data['ori_z']
 
     cur.execute('UPDATE player_info \
             SET mot_x = %s, mot_y = %s, mot_z = %s, ori_x = %s, ori_y = %s, ori_z = %s \
